1e309290bb0d3740170224f6699876aaa6857276
Waqas Shakeel <developer.waqas@gmail.com>
2023-07-25 12:15:41 +0400

    added author

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        const connect = mongoose.connect(process.env.CONNECTION_STRING);
-        const connection = (await connect).connection;
-        console.log("Database connected! ", connection.host, connection.name);
-    } catch (err) {
-        console.log(err);
-        process.exit(1);
-    }
+  try {
+    const connect = mongoose.connect(process.env.CONNECTION_STRING);
+    const connection = (await connect).connection;
+    console.log("Database connected! ", connection.host, connection.name);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
